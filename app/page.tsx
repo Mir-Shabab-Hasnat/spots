f3fe79ec88a6900efbadae6891ece965e0c1a6a6
Mir-Shabab-Hasnat <shababhasnat@gmail.com>
2025-01-08 17:44:24 -0600

    finished hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
 import PageWrapper from "@/components/container/page-wrapper";
+import HeroSection from "@/components/homepage/hero";
 import Image from "next/image";
 
 export default function Home() {
   return (
     <PageWrapper>
-      Hello World
+      <HeroSection />
     </PageWrapper>
   );
 }
